5ce1b978e44238eb23b22181e6a0e47929b1dca1
Ziki <ziki.zheng@gmail.com>
2026-05-15 15:44:33 +1000

    Add: sql-aggregation-group-by-distinct - submission-1

---
+++ SQL/sql-aggregation-group-by-distinct/submission-1.sql
@@ -0,0 +1,22 @@
+CREATE TABLE products (
+    product_id INTEGER,
+    product_name TEXT,
+    store_name TEXT
+);
+
+INSERT INTO products (product_id, product_name, store_name) VALUES
+    (1, 'Laptop', 'Amazon'),
+    (2, 'Phone', 'Best Buy'),
+    (3, 'Tablet', 'Walmart'),
+    (4, 'Laptop', 'Amazon'),
+    (5, 'Phone', 'Best Buy'),
+    (6, 'Tablet', 'Walmart'),
+    (7, 'Laptop', 'Amazon'),
+    (8, 'Washing Machine', 'Best Buy'),
+    (9, 'Laptop', 'Walmart'),
+    (10, 'Phone', 'Amazon');
+-- Do not modify above this line. --
+
+SELECT DISTINCT PRODUCT_NAME, STORE_NAME
+FROM PRODUCTS
+ORDER BY PRODUCT_NAME, STORE_NAME;
