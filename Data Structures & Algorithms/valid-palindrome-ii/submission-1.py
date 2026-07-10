9c0b1922d67265358e7c8d09be47535352feb7ac
Ziki <ziki.zheng@gmail.com>
2026-07-10 16:24:03 +1000

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def is_palindrome(l, r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+        l, r = 0, len(s)-1
+        while l < r:
+            if s[l] != s[r]:
+                return is_palindrome(l+1, r) or is_palindrome(l, r-1)
+            l += 1
+            r -= 1
+        return True
