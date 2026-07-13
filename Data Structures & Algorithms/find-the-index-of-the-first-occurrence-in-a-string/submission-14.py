7606a856556749a4d85a8fc94e132dce0fb146c6
Ziki <ziki.zheng@gmail.com>
2026-07-13 18:40:20 +1000

    Add: find-the-index-of-the-first-occurrence-in-a-string - submission-14

---
+++ Data Structures & Algorithms/find-the-index-of-the-first-occurrence-in-a-string/submission-14.py
@@ -0,0 +1,18 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        s = needle + "$" + haystack
+        n = len(s)
+        z = [0] * len(s)
+        l, r = 0, 0
+        for i in range(1, n):
+            if i < r:
+                z[i] = min(r - i, z[i - l])
+            while i + z[i] < n and s[z[i]] == s[z[i] + i]:
+                z[i] += 1
+            if i + z[i] < r:
+                l, r = i, i + z[i]
+        
+        for i in range(len(needle)+1, n):
+            if z[i] == len(needle):
+                return i - len(needle) - 1
+        return -1
