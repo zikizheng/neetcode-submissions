6b5b6485e87e8d21e2a99e0f57307e1972ca751b
Ziki <ziki.zheng@gmail.com>
2026-07-15 16:43:58 +1000

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        seen = {}
+        for i, num in enumerate(nums):
+            if num in seen and i - seen[num] <= k:
+                return True
+            seen[num] = i
+        return False
