c1c8be64ca5ffe85a32ba98b908c66c37f945f80
Ziki <ziki.zheng@gmail.com>
2026-07-27 15:26:57 +1000

    Add: house-robber - submission-9

---
+++ Data Structures & Algorithms/house-robber/submission-9.py
@@ -0,0 +1,7 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        l, r = 0, 0
+        for num in nums:
+            s = max(num + l, r)
+            l, r = r, s
+        return r
