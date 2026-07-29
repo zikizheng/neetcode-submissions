f10f3521d2bc57c457a89507ca29b9545e3db0e0
Ziki <ziki.zheng@gmail.com>
2026-07-29 11:17:23 +1000

    Add: flood-fill - submission-1

---
+++ Data Structures & Algorithms/flood-fill/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        startColour = image[sr][sc]
+        stack = [[sr, sc]]
+        seen = set()
+        while stack:
+            r, c = stack.pop()
+            if image[r][c] == startColour:
+                seen.add((r, c))
+                image[r][c] = color
+                for nr, nc in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
+                    if 0 <= r + nr < len(image) and 0 <= c + nc < len(image[0]) and (r + nr, c + nc) not in seen:
+                        stack.append((r + nr, c + nc))
+        return image
