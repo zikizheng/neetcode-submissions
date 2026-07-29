2c3235c319a270c42ec82a2e311bf05a271e6329
Ziki <ziki.zheng@gmail.com>
2026-07-29 19:53:52 +1000

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        mp = {i: [] for i in range(numCourses)}
+        for a, b in prerequisites:
+            mp[a].append(b)
+        
+        visiting = set()
+        def dfs(c):
+            if mp[c] == []:
+                return True
+            if c in visiting:
+                return False
+
+            visiting.add(c)
+
+            for p in mp[c]:
+                if not dfs(p):
+                    return False
+            
+            visiting.remove(c)
+            mp[c] = []
+            return True
+        
+        for c in range(numCourses):
+            if not dfs(c): return False
+        return True
