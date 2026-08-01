3d36cd68d68ce3e3749c0922c16adabd8a29d48a
Ziki <ziki.zheng@gmail.com>
2026-08-01 11:27:10 +1000

    Add: pacific-atlantic-water-flow - submission-1

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-1.py
@@ -0,0 +1,41 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        ROWS, COLS = len(heights), len(heights[0])
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+        p = set()
+        a = set()
+        def bfs(src, s):
+            q = collections.deque(src)
+            while q:
+                for _ in range(len(q)):
+                    r, c = q.popleft()
+                    for dr, dc in directions:
+                        nr, nc = r + dr, c + dc
+                        if (0 <= nr < ROWS and 0 <= nc < COLS 
+                            and heights[r][c] <= heights[nr][nc] 
+                            and (nr,nc) not in s):
+                            s.add((nr, nc))
+                            q.append([nr, nc])
+        pac = []
+        atl = []
+        for col in range(COLS):
+            pac.append([0, col])
+            p.add((0, col))
+            atl.append([ROWS - 1, col])
+            a.add((ROWS - 1, col))
+        for row in range(ROWS):
+            pac.append([row, 0])
+            p.add((row, 0))
+            atl.append([row, COLS - 1])
+            a.add((row, COLS - 1))
+
+        bfs(pac, p)
+        bfs(atl, a)
+
+        res = []
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r, c) in p and (r, c) in a:
+                    res.append([r, c])
+        return res
