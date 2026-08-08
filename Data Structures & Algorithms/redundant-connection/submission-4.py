730e6f26003d2461f414e5d0798189359eb21826
Ziki <ziki.zheng@gmail.com>
2026-08-08 15:55:31 +1000

    Add: redundant-connection - submission-4

---
+++ Data Structures & Algorithms/redundant-connection/submission-4.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+        cnts = [0] * (n + 1)
+        adj = [[] for _ in range(n + 1)]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+            cnts[u] += 1
+            cnts[v] += 1
+        
+        q = collections.deque()
+
+        for i in range(1, n + 1):
+            if cnts[i] == 1:
+                q.append(i)
+
+        while q:
+            node = q.popleft()
+            cnts[node] -= 1
+            for nei in adj[node]:
+                cnts[nei] -= 1
+                if cnts[nei] == 1:
+                    q.append(nei)
+        print(cnts)
+        
+        for u, v in reversed(edges):
+            if cnts[u] > 0 and cnts[v] > 0:
+                return [u, v]
+        return []
